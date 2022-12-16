df884ea676fcce2bad6492e6a0b58bf81b48843e
KISOOJIN <ekf4455@naver.com>
2022-12-16 15:27:42 +0900

    Todo-App 기능 구현

---
+++ src/components/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import styled from 'styled-components';
+
+const Todolist = styled.div`
+  border: 1px solid gray;
+  background-color: lightgray;
+  margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 600px;
+`;
+
+const TodoList = ({ todo, Delete }) => {
+  return (
+    <Todolist>
+      {todo.map((item) => (
+        <TodoItem key={item.id} item={item} Delete={Delete} />
+      ))}
+    </Todolist>
+  );
+};
+
+export default TodoList;
